9cb5b3af2e17e797b40a41039d124d65736b8e2d
Michael Hoekema <michael.hoekema@noxcrew.com>
2024-08-06 13:52:18 +0200

    TommyInnit is no more

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,12 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 export default function NotFound() {
-  return (
-    <iframe
-      className="w-screen h-screen"
-      src="https://www.youtube.com/embed/ukyC7pu6WmM?autoplay=1"
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-    />
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("https://noxcrew.com");
+  }, [router]);
+
+  return null;
 }
