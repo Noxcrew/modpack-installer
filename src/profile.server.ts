1d4ef9be75d18c0c6f388657fdf1d333dfec649e
Anthony Teague <anthony.teague@noxcrew.com>
2024-08-09 21:35:00 +0100

    Remove profile

--- src/profile.server.ts
+++ src/profile.server.ts
@@ -1,8 +1,8 @@
 import type { Profile } from "./profile"
-import { mccPrivateProfile, mccProfile } from "./profiles/mcc/mcc"
+import { mccProfile } from "./profiles/mcc/mcc"
 
 /** The profiles made available by the mod loader. */
-export const PROFILES: Profile[] = [mccProfile, mccPrivateProfile]
+export const PROFILES: Profile[] = [mccProfile]
 
 /** Returns a profile based on its associated `key` attribute, or undefined if none exist with that `key`. */
 export const getProfileByKey = (key: string): Profile | undefined =>
--- src/profiles/mcc/mcc.ts
+++ src/profiles/mcc/mcc.ts
@@ -100,4 +100,3 @@ export const mccProfile: Profile = {
         },
     ],
 }
-
