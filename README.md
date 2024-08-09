c1e9adc14342d67893665b3d178359c2e407b70c
Anthony Teague <anthony.teague@noxcrew.com>
2024-08-09 16:42:40 +0100

    Finalise profiles, add first attempt at a readme

---
+++ README.md
@@ -0,0 +1,19 @@
+<p align="center">
+    <h1 align="center">Modpack Loader</h1>
+</p>
+
+<p align="center">Download and install Minecraft profiles in one click.</p>
+
+## Usage
+
+We welcome others deploying instances to host their own mod profiles.
+
+1. Fork this repository and download dependencies.
+2. Create your profile in `src/profiles/`. An example file is provided, and type-checking is available using the `Profile` interface.
+3. Include your profiles in the `PROFILES` list located at `src/profiles.server.ts`. **Only profiles in this list are accessible.**
+4. Use a local server (`yarn dev`) to test. Your profile will be available to install at `http://localhost:3000/install/<key>`.
+5. Deploy using your favourite hosting provider.
+
+## Contributions
+
+We welcome the reporting of Issues and the creation of Pull Requests.
--- src/profile.ts
+++ src/profile.ts
@@ -6,28 +6,43 @@ export type ProfileVersionLoader = "fabric"
 
 /** The profile to install. */
 export interface Profile {
+    /** Whether the profile is active (visible). */
     readonly active?: boolean
+    /** The profile's key, used to determine the URL of the installer. */
     readonly key: string
+    /** The profile's ID, used to uniquely identify it among other profiles in the launcher. */
     readonly id: string
+    /** The profile's name, used to identify it in the launcher. */
     readonly name: string
+    /** A URL to a PNG icon for the profile. */
     readonly icon: string
+    /** Minecraft and mod loader version information. */
     readonly version: ProfileVersion
+    /** Java Arguments to include in the profile (defaults are included if unspecified). */
     readonly javaArgs?: string
+    /** Servers to include in the profile's `servers.dat` file. */
     readonly servers: ProfileServer[]
+    /** Mods to include in the profile. */
     readonly mods: ProfileMod[]
+    /** Arbitrary files to include in the profile. */
     readonly files?: Record<string, string>
 }
 
 /** The version of Minecraft and the mod loader to use. */
 export interface ProfileVersion {
+    /** The loader to use in the profile. */
     readonly loader?: ProfileVersionLoader
+    /** The version of the mod loader to use. If unspecified, we use the latest version. */
     readonly loaderVersion?: string
+    /** The Minecraft version to use in the profile. */
     readonly minecraft: string
 }
 
 /** A server to include in the profile. */
 export interface ProfileServer {
+    /** The name of the server. */
     readonly name: string
+    /** The host of the server. */
     readonly host: string
 }
 
@@ -36,10 +51,16 @@ export type ProfileModCategory = "optimization" | "enhancement" | "other"
 
 /** The mod to install. */
 export interface ProfileMod {
+    /** Whether the mod is required, recommended, or optional. */
     readonly option: ComponentInstallOption
+    /** The name of the mod to show in the UI. */
     readonly name: string
+    /** A description of the mod to show in a tooltip, if included.. */
     readonly description?: string
+    /** Where to store the mod in the profile. */
     readonly path: string
+    /** A URL indicating where to download the mod from. */
     readonly url: string
+    /** The mod's category. */
     readonly category?: ProfileModCategory
 }
